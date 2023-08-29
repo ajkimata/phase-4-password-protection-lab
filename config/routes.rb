e7ee405df19a0309ba5cc90f3280b95ae5c2e23a
alfred kimata <alfyjason@gmail.com>
2023-08-29 13:19:48 +0300

    assignment complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post "/login", to: "session#create"
+  delete "/logout", to: "session#destroy"
+  # get "/index", to: "session#index"
+
+  post "/signup", to: "users#create"
+  get "/me", to: "users#show"
 end
